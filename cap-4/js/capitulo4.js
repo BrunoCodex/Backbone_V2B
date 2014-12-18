4b99df9c167498baa68dc4dd08997855ffe3fc45
Bruno Sanchez Oviedo <bso_2013@hotmail.com>
2014-12-18 11:40:59 -0500

    Indentacion
    Indentacion de todo el codigo

--- cap-4/js/capitulo4.js
+++ cap-4/js/capitulo4.js
@@ -1,57 +1,56 @@
 var paneles;
 var num_paneles;
-$(document).ready(function(){
-  
-  var PanelesCollection = Backbone.Collection.extend(
-    {
-      model:Panel
-    }
-  );
+    $(document).ready(function(){
+        var PanelesCollection = Backbone.Collection.extend(
+            {
+              model:Panel
+            }
+        );
 
-  paneles = new PanelesCollection(
-    [
-      {txt:"este es el panel 1", rotulo:"Panel 1", id:"1"},
-      {txt:"este es el panel 2", rotulo:"Panel 2", id:"2"},
-      {txt:"este es el panel 3", rotulo:"Panel 3", id:"3"}
-    ]
-  );
+        paneles = new PanelesCollection(
+            [
+              {txt:"este es el panel 1", rotulo:"Panel 1", id:"1"},
+              {txt:"este es el panel 2", rotulo:"Panel 2", id:"2"},
+              {txt:"este es el panel 3", rotulo:"Panel 3", id:"3"}
+            ]
+        );
 
-  onChangePanels(null,null);
+        onChangePanels(null,null);
 
-  paneles.on({"add":onChangePanels, "remove":onChangePanels});
+        paneles.on({"add":onChangePanels, "remove":onChangePanels});
 
-  trace(JSON.stringify(paneles.toJSON()));
+        trace(JSON.stringify(paneles.toJSON()));
 
-  /*$("#create_button").click(function(){
-    // alert("Hola");
-    paneles.add({txt:"este es el panel 4", rotulo:"Panel 4", id:"4"});
-  });*/
+        /*$("#create_button").click(function(){
+            alert("Hola");
+            paneles.add({txt:"este es el panel 4", rotulo:"Panel 4", id:"4"});
+        });*/
 
-  num_paneles = paneles.length + 1;
+        num_paneles = paneles.length + 1;
 
-  
-  $("#create_button").click(function(){
-    // alert("Hola");
-    //paneles.add({txt:"este es el panel 4", rotulo:"Panel 4", id:"4"});
-    /*var panel = new Panel({txt:"este es el panel 4", rotulo:"Panel 4", id:"4"});
-    paneles.add(panel);
-    paneles.add({txt:"este es el panel 5", rotulo:"Panel 5", id:"5"})*/
-    //trace(JSON.stringify(paneles.toJSON()));
-    /*trace(paneles);*/
+      
+        $("#create_button").click(function(){
+            // alert("Hola");
+            //paneles.add({txt:"este es el panel 4", rotulo:"Panel 4", id:"4"});
+            /*var panel = new Panel({txt:"este es el panel 4", rotulo:"Panel 4", id:"4"});
+            paneles.add(panel);
+            paneles.add({txt:"este es el panel 5", rotulo:"Panel 5", id:"5"})*/
+            //trace(JSON.stringify(paneles.toJSON()));
+            /*trace(paneles);*/
 
 
-    paneles.add({txt:"este es el panel "+num_paneles, rotulo:"Panel "+num_paneles, id: num_paneles});
-    num_paneles++;
-  });
+            paneles.add({txt:"este es el panel "+num_paneles, rotulo:"Panel "+num_paneles, id: num_paneles});
+            num_paneles++;
+        });
 
-  $("#delete_button").click(function(){
-    paneles.remove(paneles.at(0));
-  });
+        $("#delete_button").click(function(){
+            paneles.remove(paneles.at(0));
+        });
 
-  $("#delete_button_ID").click(function(){
-    paneles.remove(paneles.get($("#rot_del").val()));
-  });
-});
+        $("#delete_button_ID").click(function(){
+            paneles.remove(paneles.get($("#rot_del").val()));
+        });
+    });
 
 function onChangePanels(model, collection){
   // debugger;
